39d13ebd1c916f869b010a535b387475b23e7b0d
Theodore Riera <warsang@amazon.com>
2021-02-03 16:20:57 +0000

    Reverted if/else change back to value 10

--- Investigation Lab.py
+++ Investigation Lab.py
@@ -93,7 +93,7 @@ def high_positives(action=None, success=None, container=None, results=None, hand
         container=container,
         action_results=results,
         conditions=[
-            ["file_reputation_1:action_result.summary.positives", ">", 90],
+            ["file_reputation_1:action_result.summary.positives", ">", 10],
         ])
 
     # call connected blocks if condition 1 matched
